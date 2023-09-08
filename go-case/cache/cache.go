cf3ae549de70d6128591b679aa0dfdd28dc8e523
lanjin.wei <137482324@qq.com>
2023-09-08 15:17:35 +0800

    feat : cache

--- go-case/cache/cache.go
+++ go-case/cache/cache.go
@@ -1,20 +1,74 @@
 package cache
 
-import "time"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// https://zhuanlan.zhihu.com/p/635603181
 
 type Cache interface {
-	// size : 1KB 100KB 1ME 2MB 1GB
+	// SetMaxMemory size : 1KB 100KB 1M 2MB 1GB
 	SetMaxMemory(size string) bool
 
-	Set(key string, val any, expire time.Duration) bool
+	Set(key string, val any, expire time.Duration) error
 
-	Get(key string) (any, bool)
+	Get(key string, result any) error
 
 	Del(key string) bool
 
 	Exists(key string) bool
 
-	Flush() bool
-	// 获取所有缓存中 key 的数量
+	Clear() bool
+	// Keys 获取所有缓存中 key 的数量
 	Keys() int64
+}
+
+var defaultSize = "512M"
+
+func DefaultCache() (Cache, error) {
+	return newFreeCache(defaultSize)
+}
+
+const (
+	FreeCacheEngine = "freeCache"
+)
+
+func New(engine, maxSize string) (Cache, error) {
+	switch engine {
+	case FreeCacheEngine:
+		return newFreeCache(maxSize)
+	default:
+		return newFreeCache(maxSize)
+	}
+}
+
+func parseUnit(memSize string) (int, error) {
+
+	re, _ := regexp.Compile("[0-9]+")
+	loc := re.FindStringIndex(memSize)
+	if len(loc) != 2 {
+		return 0, fmt.Errorf("unit parse not exist: %s len(loc) = %d", memSize, len(loc))
+	}
+	num, _ := strconv.Atoi(memSize[:loc[1]])
+	unit := strings.ToUpper(memSize[loc[1]:])
+	switch unit {
+	case "B":
+		return num, nil
+	case "KB", "K":
+		return num << 10, nil
+	case "MB", "M":
+		return num << 20, nil
+	case "GB", "G":
+		return num << 30, nil
+	case "TB", "T":
+		return num << 40, nil
+	case "PB", "P":
+		return num << 50, nil
+	default:
+		return 0, fmt.Errorf("unit parse not exist: %s -> %s", memSize, memSize[loc[1]:])
+	}
 }
--- go-case/cache/cache_test.go
+++ go-case/cache/cache_test.go
@@ -1,9 +1,56 @@
 package cache_test
 
 import (
+	"lovec.wlj/go-case/cache"
 	"regexp"
 	"testing"
+	"time"
 )
+
+func TestCache(t *testing.T) {
+	che, err := cache.DefaultCache()
+
+	if err != nil {
+		t.Error(err)
+	}
+	if err = che.Set("foo", "bar", 3*time.Second); err != nil {
+		t.Error(err)
+	}
+	if err = che.Set("foo1", "bar1", time.Second); err != nil {
+		t.Error(err)
+	}
+	t.Log(che.Keys())
+
+	var value string
+	err = che.Get("foo1", &value)
+	t.Log(value)
+
+	var value1 string
+	che.Del("foo")
+	err = che.Get("foo", &value1)
+	t.Log(value1)
+
+	var value2 string
+	time.Sleep(2 * time.Second)
+	err = che.Get("foo1", &value2)
+	t.Log(value2)
+
+	che.Clear()
+	t.Log(che.Keys())
+
+	// --------------------------------
+
+	m := map[string]any{
+		"name": "wlj",
+		"age":  23,
+	}
+	if err := che.Set("hash", m, 3*time.Second); err != nil {
+		t.Error(err)
+	}
+	var hv map[string]any
+	err = che.Get("hash", &hv)
+	t.Log(hv)
+}
 
 // 解析出单位
 func TestParseUnit(t *testing.T) {
