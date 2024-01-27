77d1a3ca8ca9d82c1981e263a520934f0ea37aaf
lanjin.wei <137482324@qq.com>
2024-01-27 19:46:43 +0800

    feat: redis key slowlog

--- example/redis/key/keys_scan_test.go
+++ example/redis/key/keys_scan_test.go
@@ -1,15 +1,23 @@
-package basis
+package key
 
 import (
-	"context"
 	"log"
 	"testing"
 )
 
+// 问题 1：新增的 key 可能没有遍历到
+// 问题 2：遍历出了重复键
+//
+// keys 	--> scan
+// hgetall 	--> hscan
+// smembers --> sscan
+// zrange 	--> zscan
+
 func TestKeys(t *testing.T) {
-	ctx := context.Background()
 
 	// keys pattern
+	// * -- 比配任意字符
+	// . -- 比配一个字符
 	// e.g keys *
 	//	   keys user:[0-9]* # 查找以 "user:" 开头且包含数字的所有键
 	// keys命令会遍历所有键，所以它的时间复杂度是O（n）, 并且将匹配模式的键一次性地返回给客户端
@@ -19,15 +27,14 @@ func TestKeys(t *testing.T) {
 }
 
 func TestScan(t *testing.T) {
-	ctx := context.Background()
 
-	// SCAN cursor match count
+	// SCAN cursor match count [type]
 	// e.g SCAN 0 MATCH user:* COUNT 10 # 使用 SCAN 迭代查找以 "user:" 开头的所有键，并每次返回 10 个键
 	//
 	// scan命令使用游标（cursor）方式来逐步迭代匹配的键。它将匹配的键分批返回，以减轻服务的阻塞和性能压力。
 	// SCAN命令的算法是基于近似随机采样的算法。它使用一个游标来记录当前迭代的位置，并在每次迭代时返回一批键。
 	// 迭代过程中，服务器会根据游标位置和一定的采样数量来获取匹配的键。
-	iter := rdb.Scan(ctx, 0, "*", 0).Iterator()
+	iter := rdb.ScanType(ctx, 0, "*", 0, "string").Iterator()
 	for iter.Next(ctx) {
 		log.Println("key:", iter.Val())
 	}
@@ -47,4 +54,14 @@ func TestScan(t *testing.T) {
 	// if err != nil {
 	//	 panic(err)
 	// }
+}
+
+func TestScanZset(t *testing.T) {
+	iter := rdb.ZScan(ctx, "zsetkey128", 0, "*", 10).Iterator()
+	for iter.Next(ctx) {
+		log.Println("key:", iter.Val())
+	}
+	if err := iter.Err(); err != nil {
+		panic(err)
+	}
 }
---
+++ example/redis/manager/slowlog_test.go
@@ -0,0 +1,39 @@
+package manager
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"log"
+	"lovec.wlj/example/redis/initialize"
+	"testing"
+)
+
+var (
+	rdb redis.UniversalClient
+	ctx = context.Background()
+)
+
+func init() {
+	rdb = initialize.Redis()
+}
+
+func TestSlowLog(t *testing.T) {
+
+	// slowlog-log-slower-than = 10000 us (微妙) = 10ms【 =0 记录所有的命令，<0 不会记录 】
+	// slowlog-max-len 慢查询日志最多存储多少条
+
+	// config set slowlog-log-slower-than 20000
+	// config set slowlog-max-len 1000
+	// config rewrite 将配置持久化到本地配置文件
+	rdb.ConfigSet(ctx, "slowlog-log-slower-than", "0")
+	slowLogs, _ := rdb.SlowLogGet(ctx, 10).Result()
+	for _, sl := range slowLogs {
+		// 慢查询只记录命令执行时间
+		// 慢查询日志是一个先进先出的队列
+		// 可能会丢失部分慢查询命令
+		log.Println("慢查询日志标识ID:", sl.ID, " 发生时间戳:", sl.Time, " 命令耗时:", sl.Duration, " 执行命令和参数:", sl.Args)
+	}
+
+	// slowlog len 获取慢查询日志列表当前的长度
+	// slowlog reset 慢查询日志重置 (对列表做清理操作)
+}
