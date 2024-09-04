fdff0fd35e3e8127a094bb7647869c9da41e1b12
lanjin.wei <137482324@qq.com>
2024-09-04 23:19:22 +0800

    redis: cmd scan

---
+++ example/redis/cmdscan/scan_test.go
@@ -0,0 +1,44 @@
+package cmdscan
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+	"lovec.wlj/example/redis/initialize"
+)
+
+var (
+	rdb redis.UniversalClient
+	ctx = context.Background()
+)
+
+func init() {
+	rdb = initialize.Redis()
+}
+
+func TestScan(t *testing.T) {
+	var total int
+	var (
+		keys []string
+		cur  uint64
+		err  error
+	)
+	for {
+		keys, cur, err = rdb.Scan(ctx, cur, "*", 2).Result()
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+		total += len(keys)
+
+		slog.Info("result ", "keys", keys, "cur", cur)
+		if cur == 0 {
+			break
+		}
+	}
+
+	log.Println("count", total)
+}
